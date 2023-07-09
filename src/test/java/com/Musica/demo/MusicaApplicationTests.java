d9ffc44c05adc1c8cd833da1a711a32fcec3eb5b
code- <code-@DESKTOP-17LQHR3>
2023-07-09 15:08:09 -0600

    Primer avance de proyecto

---
+++ src/test/java/com/Musica/demo/MusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/Musica/demo/controller/controller.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Musica.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+/**
+ *
+ * @author code-
+ */
+@Controller
+public class controller {
+   public String goHome(Model model){
+       model.addAttribute("titulo","Bienvenido, pagina de inicio");
+       return "inicio";
+   }
+}
